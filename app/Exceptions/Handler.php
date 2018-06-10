9ebad8c915b72fb6087c197c2d7d904539806c95
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 19:52:48 +0530

    Handle model not found exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,15 +5,15 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
+
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
     protected $dontReport = [
-        //
+            //
     ];
 
     /**
@@ -32,8 +32,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
-    {
+    public function report(Exception $exception) {
         parent::report($exception);
     }
 
@@ -44,8 +43,13 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
-    {
+    public function render($request, Exception $exception) {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json([
+                        'error' => 'Resource not found'
+                            ], 404);
+        }
         return parent::render($request, $exception);
     }
+
 }
