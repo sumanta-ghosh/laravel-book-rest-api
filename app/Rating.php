4e948ad8fea06cc462378704edd381b30e78dd3c
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 18:38:10 +0530

    Add comments in rating model

--- app/Rating.php
+++ app/Rating.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model {
 
+    /**
+     * The attributes that are mass assignable 
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'book_id', 'user_id', 'rating'
+    ];
+
     public function book() {
         return $this->belongsTo(Book::class);
     }
