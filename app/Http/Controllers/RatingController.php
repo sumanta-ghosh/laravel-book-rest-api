26e771d35ae3bed8747a981054c31006448c765f
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 19:35:58 +0530

    Creating the rating controller

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+use App\Rating;
+use App\Http\Resources\RatingResource;
+
+class RatingController extends Controller {
+
+    public function store(Request $request, Book $book) {
+        $rating = Rating::firstOrCreate([
+                    'user_id' => $request->user()->id,
+                    'book_id' => $book->id], ['rating' => $request->rating]
+        ); 
+
+        return new RatingResource($rating);
+    }
+
+}
