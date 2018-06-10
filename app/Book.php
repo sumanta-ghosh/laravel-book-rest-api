2383c630ef8b9d919a76aa95ed729c0fecbb186d
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 18:16:16 +0530

    Define relationships between User,Book and Rating models

--- app/Book.php
+++ app/Book.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
-{
-    //
+class Book extends Model {
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rattings() {
+        return $this->hasMany(Ratings::class);
+    }
+
 }
--- app/Rating.php
+++ app/Rating.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Rating extends Model
-{
-    //
+class Rating extends Model {
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
+
 }
