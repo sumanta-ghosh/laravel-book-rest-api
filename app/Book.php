cc6d76f69388fe010bf49e4b706eccf3473ab161
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 18:33:20 +0530

    Allowing mass assignment on some fields in Book model

--- app/Book.php
+++ app/Book.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model {
 
+    /**
+     *The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'title', 'description'
+    ];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
