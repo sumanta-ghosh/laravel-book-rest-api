64b5a0b65a925c388971f4f4fc5a78f13c947e71
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-06-10 18:45:51 +0530

    Defining API routes

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
+
+Route::apiResource('books', 'BookController');
+Route::post('books/{book}/ratings', 'RatingController@store');
